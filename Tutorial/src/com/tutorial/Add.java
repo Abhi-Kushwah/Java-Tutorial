ab12851cba9743c25477e6832be50e070e5608d0
HP <abhishekkushwah75094@gmail.com>
2025-02-13 22:54:42 +0300

    addition

---
+++ Tutorial/src/com/tutorial/Add.java
@@ -0,0 +1,11 @@
+package com.tutorial;
+
+public class Add {
+	public static void main(String[] args) {
+		int x=10;
+		int y=10;
+		int c= x+y;
+	System.out.println("Add two number sum :"+c);
+	}
+
+}
